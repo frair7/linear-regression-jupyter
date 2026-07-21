cc758224fa92cfee8ce911127c7b2159efe5ceb9
Emma Frair <frair7@ascend-rw02.ten.osc.edu>
2026-07-20 21:02:24 -0400

    Update manual regression scripts, notebooks, and outputs

--- manual/linear_regression_python.py
+++ manual/linear_regression_python.py
@@ -20,6 +20,13 @@ intercept = np.ravel(model.intercept_)[0]
 predictions = model.predict(dataset[[x_column]])
 r2 = r2_score(dataset[y_column], predictions)
 
+years_to_predict = 6
+prediction_input = pd.DataFrame(
+    {x_column: [years_to_predict]}
+)
+
+predicted_salary = model.predict(prediction_input)[0][0]
+
 plt.scatter(dataset["YearsExperience"], dataset["Salary"], color=(1, 0, 0, 0.5), edgecolors="k")
 plt.plot(dataset["YearsExperience"], model.predict(dataset[["YearsExperience"]]), color="blue")
 plt.grid(True, color="gray", linestyle="--", linewidth=0.5, alpha=0.7, zorder=0)
@@ -29,11 +36,30 @@ plt.xlabel("Years of Experience", fontsize=12)
 plt.ylabel("Salary", fontsize=12)
 plt.text(
     1.1,
-    60000,
-    f"y = {slope:.2f}x + {intercept:.2f}\n$R^2 = 0.79$",
+    70000,
+    f"y = {slope:.2f}x + {intercept:.2f}\n"
+    f"$R^2$ = {r2:.2f}\n", 
     fontsize=11
 )
+
+plt.scatter(
+    years_to_predict,
+    predicted_salary,
+    color="green",
+    s=100,
+    marker="*",
+    label="Predicted Salary at 6Y"
+)
+plt.text(
+    5.3,
+    75000,
+    f"${predicted_salary:,.2f}",
+    fontsize=11,
+    color="green"
+)
+plt.legend()
 plt.savefig(f"manual/{y_column}_vs_{x_column}.png", dpi=300)
 plt.show()
 
 print("R-squared:", model.score(dataset[[x_column]], dataset[[y_column]]))  # R-squared
+print(f"Predicted salary for {years_to_predict} years of experience: ${predicted_salary:,.2f}")
